8eaf54b14c061746728b03425902f9c145b12ae1
SirBatoio <116562702+SirBatoio@users.noreply.github.com>
2023-05-06 10:55:01 +0200

    nuovo posto per l'output

--- js/map.js
+++ js/map.js
@@ -32,14 +32,6 @@ function loadMap() {
     const userMarker = L.marker([lat, lon]).addTo(map);
     userMarker.bindPopup("Posizione dell'utente");
 
-    if (data) {
-      data.stationBeanList.forEach((station) => {
-        const marker = L.marker([station.latitude, station.longitude]).addTo(
-          map
-        );
-        marker.bindPopup(station.stationName);
-      });
-    }
   });
 
   var marker;
