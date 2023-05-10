4119a74ece0611c38aefc30acc5fa7492243980b
SirBatoio <116562702+SirBatoio@users.noreply.github.com>
2023-05-10 11:21:02 +0200

    tutti per uno

--- js/app.js
+++ js/app.js
@@ -92,3 +92,61 @@ function formatDistance(num) {
     return num + " m";
   }
 }
+
+function loadMap() {
+  var map = L.map("map").setView([40.7128, -74.006], 14);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+    minZoom: 12,
+  }).addTo(map);
+
+  fetch("citybike.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const customIcon = L.icon({
+        iconUrl: "img/pos.png",
+
+        iconSize: [55, 55],
+
+        iconAnchor: [29, 49],
+      });
+      data.stationBeanList.forEach((station) => {
+        const marker = L.marker([station.latitude, station.longitude], {
+          icon: customIcon,
+        }).addTo(map);
+        marker.bindPopup(station.stationName);
+      });
+    })
+    .catch((error) => console.error(error));
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const userMarker = L.marker([lat, lon]).addTo(map);
+    userMarker.bindPopup("Posizione dell'utente");
+
+  });
+
+  var marker;
+
+  function onMapClick(e) {
+    if (marker) {
+      map.removeLayer(marker);
+    }
+    const userIcon = L.icon({
+      iconUrl: "img/userpos.png",
+
+      iconSize: [55, 55],
+
+      iconAnchor: [29, 49],
+    });
+    marker = L.marker(e.latlng, { icon: userIcon }).addTo(map);
+    document.getElementById("lat1").value = e.latlng.lat;
+    document.getElementById("lon1").value = e.latlng.lng;
+  }
+
+  map.on("click", onMapClick);
+}
+
